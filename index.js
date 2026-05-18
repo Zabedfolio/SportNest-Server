3d8f7f39fa143c3aa655c4a6115f027b52d144cd
Zabedfolio <zabedfolio@gmail.com>
2026-05-18 17:11:13 +0600

    info added success

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv');
 dotenv.config();
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const uri = process.env.MONGODB_URI;
@@ -18,14 +22,25 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+
+    const db = client.db("SportNest");
+    const facilityCollection = db.collection("facilities");
+
+    app.post('/facilities', async (req, res) => {
+        const facilityData = req.body;
+        console.log(facilityData);
+        const result = await facilityCollection.insertOne(facilityData);
+        res.send(result);
+    });
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
