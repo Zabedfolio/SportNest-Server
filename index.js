238e137fce665ddc32de5430a106f3c5a4cb0617
Zabedfolio <zabedfolio@gmail.com>
2026-05-20 16:07:10 +0600

    added

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.use(express.json());
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const uri = process.env.MONGODB_URI;
-
 const PORT = process.env.PORT || 5000;
 
 const client = new MongoClient(uri, {
@@ -24,18 +23,31 @@ async function run() {
   try {
     await client.connect();
 
-
     const db = client.db("SportNest");
     const facilityCollection = db.collection("facilities");
 
-    app.post('/facilities', async (req, res) => {
-        const facilityData = req.body;
-        console.log(facilityData);
-        const result = await facilityCollection.insertOne(facilityData);
+    app.get('/facilities', async (req, res) => {
+        const { search, type } = req.query;
+
+        const query = {};
+
+        if (search) {
+            query.facilityName = { $regex: search, $options: 'i' };
+        }
+
+        if (type && type !== 'all') {
+            query.facilityType = { $in: type.split(',') };
+        }
+
+        const result = await facilityCollection.find(query).toArray();
         res.send(result);
     });
 
-
+    app.post('/facilities', async (req, res) => {
+        const facilityData = req.body;
+        const result = await facilityCollection.insertOne(facilityData);
+        res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -45,14 +57,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('Server is running');
-})
+});
 
-// SportNest
-// pass: VrqsXkRm3lN1JaEE
-
-
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`${PORT} is running`);
-})
+});
